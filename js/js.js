a202ac7b270e2a0a5802071e95c784c6cfe7e07a
ANTON POSOKHIN <34386496+posokhin@users.noreply.github.com>
2018-08-01 17:24:42 +0300

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,6 +1,22 @@
-var btn = $('.fas');
+var btn = $('.burger');
 var menu = $('.sidebar__menu');
 
-$(btn).click(function(){
+$(btn).on('click', function () {
     $(menu).slideToggle('sidebar_menu');
+})
+
+var scrollbtn = $('.scrollbtn');
+
+$(window).on('scroll', function () {
+    if ($(this).scrollTop() > 200) {
+        $('.scrollbtn').fadeIn();
+    } else {
+        $('.scrollbtn').fadeOut();
+    }
+})
+$('.scrollbtn').on('click', function (e) {
+    e.preventDefault();
+    $('html , body').animate({
+        scrollTop: 0
+    } , 800);
 })
